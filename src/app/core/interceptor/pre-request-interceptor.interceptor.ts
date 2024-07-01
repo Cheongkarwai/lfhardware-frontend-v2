d71fecf9d65771334a15e72d7549354c1a15f340
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-07-02 00:12:01 +0800

    login if 401 occurs, uncomment for now first

--- src/app/core/interceptor/pre-request-interceptor.interceptor.ts
+++ src/app/core/interceptor/pre-request-interceptor.interceptor.ts
@@ -1,9 +1,22 @@
 import {HttpInterceptorFn} from '@angular/common/http';
+import {LoginService} from "../user/login.service";
+import {inject} from "@angular/core";
+import {catchError, of} from "rxjs";
 
 
 export const preRequestInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
 
-  return next(req);
+  const loginService: LoginService = inject(LoginService);
+
+
+  return next(req)
+  //   .pipe(catchError((res)=>{
+  //   if(res.status === 401){
+  //     loginService.login();
+  //     return of(res);
+  //   }
+  //   return of(res);
+  // }));
   // let authReq = req.clone({headers:new HttpHeaders().set('Authorization',`Bearer ${result}`)});
   // return next(authReq);
 
